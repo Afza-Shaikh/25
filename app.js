933d15e0a66f27be6083eaebc1db2aca87bfc2f2
afza shaikh <shaikhafza961@gmail.com>
2024-09-16 22:25:25 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict";
+let alien_varaible = "green";
+//passing version
+//if (alien_varaible === "green"){
+// console.log(`player just earn 5 points`.);
+//}
+//else{
+//console.log(`no points earn`)
+//}
+//falling version
+alien_varaible = "red";
+if (alien_varaible === 'green') {
+    console.log(`player just earn 5 point.`);
+}
+else {
+    console.log(`{}`);
+}
